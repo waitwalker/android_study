45f317a2328104f982d38b57743e367a68e68057
waitwalker <waitwalker@163.com>
2020-07-31 15:51:42 +0800

    按钮相关状态&圆角&边框等设置

---
+++ day03Button/app/src/main/java/com/example/day03button/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.day03button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("按钮");
+            }
+        });
+    }
+}
