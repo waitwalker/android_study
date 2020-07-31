647f7456ea1a0c87d704a559ad59338437473ff9
waitwalker <waitwalker@163.com>
2020-07-31 16:48:20 +0800

    单选框学习

--- day03RadioButton/app/src/main/java/com/example/day03radiobutton/MainActivity.java
+++ day03RadioButton/app/src/main/java/com/example/day03radiobutton/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.day03radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -14,12 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RadioGroup radioGroup = findViewById(R.id.radio_group);
+        final RadioGroup radioGroup = findViewById(R.id.radio_group);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton radioButton = findViewById(checkedId);
                 Toast.makeText(getApplicationContext(),"你选择了:" + radioButton.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button publishButton = findViewById(R.id.btn);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < radioGroup.getChildCount(); i++) {
+                    RadioButton radioButton = (RadioButton) radioGroup.getChildAt(i);
+                    if (radioButton.isChecked()) {
+                        Toast.makeText(getApplicationContext(),"你选的的是:"+radioButton.getText(),Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                }
             }
         });
     }
