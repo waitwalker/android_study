1bbf29ae9ba1db71e1a444ab226c815e94b792c3
waitwalker <waitwalker@163.com>
2020-07-31 16:39:05 +0800

    RadioButton 选择框处理

---
+++ day03RadioButton/app/src/main/java/com/example/day03radiobutton/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.day03radiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(getApplicationContext(),"你选择了:" + radioButton.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
