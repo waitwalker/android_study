c85c840c814d83410d25fa666b15999ba98f3071
waitwalker <waitwalker@163.com>
2020-08-01 15:29:08 +0800

    SeekBar进度条学习

---
+++ day04SeekBar/app/src/main/java/com/etiantian/day04seekbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.etiantian.day04seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seek);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(getApplicationContext(),"进度显示" + progress, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"按下SeekBar",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"松开SeekBar",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
