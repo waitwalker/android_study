4bd73bdb0b5b70daa29935444f5a2b93281a7730
waitwalker <waitwalker@163.com>
2020-08-01 16:10:41 +0800

    计时器Chromometer学习

--- day04TextClock/app/src/main/java/com/etiantian/day04textclock/MainActivity.java
+++ day04TextClock/app/src/main/java/com/etiantian/day04textclock/MainActivity.java
@@ -3,6 +3,7 @@ package com.etiantian.day04textclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Chronometer chronometer = findViewById(R.id.chro);
+        chronometer.start();
     }
 }
