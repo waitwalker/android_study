238d6fcf6a157c19f7a561e7491f0ce4d26d4c8f
waitwalker <waitwalker@163.com>
2020-08-01 16:23:19 +0800

    日期选择器DatePicker学习

---
+++ day04DatePicker/app/src/main/java/com/etiantian/day04datepicker/MainActivity.java
@@ -0,0 +1,30 @@
+package com.etiantian.day04datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatePicker datePicker = findViewById(R.id.date);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(),year + "年" + monthOfYear + "月" + dayOfMonth + "日", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
