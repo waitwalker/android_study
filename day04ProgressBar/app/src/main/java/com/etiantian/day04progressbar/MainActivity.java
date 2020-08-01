02bde309a8a6a0bac29e18e998391519088a6e34
waitwalker <waitwalker@163.com>
2020-08-01 15:17:30 +0800

    progressbar 相关属性学习

--- day04ProgressBar/app/src/main/java/com/etiantian/day04progressbar/MainActivity.java
+++ day04ProgressBar/app/src/main/java/com/etiantian/day04progressbar/MainActivity.java
@@ -1,14 +1,65 @@
 package com.etiantian.day04progressbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    int maxProgress = 200;
+    int currentProgress = 0;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    progressBar.setProgress(currentProgress);
+                    break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progress2);
+        maxProgress = progressBar.getMax();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (true) {
+                    try {
+                        for (int i = 0; i <=200; i++) {
+                            Thread.sleep(1000);
+                            currentProgress = +10;
+                            if (currentProgress > maxProgress) {
+                                break;
+                            }
+                            handler.sendEmptyMessage(0);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 }
