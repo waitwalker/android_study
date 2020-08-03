34cdeab4119ac03ec7165dfb40533f5b998dae7b
waitwalker <waitwalker@163.com>
2020-08-03 14:26:05 +0800

    Activity显示&隐式启动

---
+++ day06Activity/app/src/main/java/com/example/day06activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.day06activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.day06activity.ThirdActivity");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ day06Activity/app/src/main/java/com/example/day06activity/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.example.day06activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
