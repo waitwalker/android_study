38663641c04bfe68f013ff9cf5c4d3eaaae8e063
waitwalker <waitwalker@163.com>
2020-08-04 17:21:54 +0800

    动态加载Fragment

--- day08Fragment/app/src/main/java/com/example/day08fragment/MainActivity.java
+++ day08Fragment/app/src/main/java/com/example/day08fragment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.day08fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyFragment2 fragment2 = new MyFragment2();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.rl_fragment, fragment2);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
--- day08Fragment/app/src/main/java/com/example/day08fragment/MyFragment2.java
+++ day08Fragment/app/src/main/java/com/example/day08fragment/MyFragment2.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MyFragment extends Fragment {
+public class MyFragment2 extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_layout, container, false);
+        return inflater.inflate(R.layout.fragment_layout2, container,false);
     }
 }
