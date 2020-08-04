fd152d704fc117ea2a53110944c6067b5e445a09
waitwalker <waitwalker@163.com>
2020-08-04 16:44:23 +0800

    添加通讯录处理

--- day07ContentProvider/app/src/main/java/com/example/day07contentprovider/MainActivity.java
+++ day07ContentProvider/app/src/main/java/com/example/day07contentprovider/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -17,6 +18,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnClickListener {
 
@@ -70,6 +74,45 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         int permission = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_CONTACTS);
         if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CONTACTS},0);
+        }
+
+        try {
+            Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
+            Uri dataUri = Uri.parse("content://com.android.contacts/data");
+            ContentResolver resolver = getContentResolver();
+            ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>();
+            ContentProviderOperation operation1 = ContentProviderOperation.newInsert(uri)
+                    .withValue("account_name",null)
+                    .build();
+            operations.add(operation1);
+
+            ContentProviderOperation operation2 = ContentProviderOperation.newInsert(dataUri)
+                    .withValueBackReference("raw_contact_id",0)
+                    .withValue("mimetype","vnd.android.cursor.item/name")
+                    .withValue("data2","马云")
+                    .build();
+            operations.add(operation2);
+
+            ContentProviderOperation operation3 = ContentProviderOperation.newInsert(dataUri)
+                    .withValueBackReference("raw_contact_id",0)
+                    .withValue("mimetype","vnd.android.cursor.item/phone_v2")
+                    .withValue("data1","13688886666")
+                    .withValue("data2","2")
+                    .build();
+            operations.add(operation3);
+
+            ContentProviderOperation operation4 = ContentProviderOperation.newInsert(dataUri)
+                    .withValueBackReference("raw_contact_id",0)
+                    .withValue("mimetype","vnd.android.cursor.item/email_v2")
+                    .withValue("data1","13688886666@163.com")
+                    .withValue("data2","2")
+                    .build();
+            operations.add(operation4);
+
+            resolver.applyBatch("com.android.contacts",operations);
+            Toast.makeText(getApplicationContext(),"新增通讯录成功",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Log.d("1","ee" + e.getLocalizedMessage());
         }
     }
 }
