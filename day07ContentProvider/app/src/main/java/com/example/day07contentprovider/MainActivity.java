bd025878b301fa14d6cceef8fc87b8a2c58a2699
waitwalker <waitwalker@163.com>
2020-08-04 15:23:31 +0800

    ContentProvider 学习使用

---
+++ day07ContentProvider/app/src/main/java/com/example/day07contentprovider/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.day07contentprovider;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new  String[]{Manifest.permission.READ_CONTACTS}, 0);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                getContact();
+                break;
+            case R.id.button2:
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void getContact() {
+        ContentResolver resolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+        Cursor cursor = resolver.query(uri,null,null,null);
+        while (cursor.moveToNext()) {
+            String cName =cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String cNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            Log.d("1","姓名:"+ cName);
+            Log.d("1","号码:"+ cNum);
+            Log.d("1","===================");
+        }
+        cursor.close();
+    }
+
+    private void addContact() {
+
+        int permission = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_CONTACTS);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+
+        }
+    }
+}
