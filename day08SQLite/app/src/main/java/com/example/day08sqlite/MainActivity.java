e4d6e8a3076647f16e479cd9c0bba54650bab810
waitwalker <waitwalker@163.com>
2020-08-05 10:53:22 +0800

    更新&查询数据

--- day08SQLite/app/src/main/java/com/example/day08sqlite/MainActivity.java
+++ day08SQLite/app/src/main/java/com/example/day08sqlite/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -31,12 +32,13 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
         Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
 
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
-
+        button5.setOnClickListener(this);
     }
 
     @Override
@@ -46,15 +48,32 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             case R.id.button:
                 // 插入
                 ContentValues values = new ContentValues();
-                values.put("name","张三丰");
+                values.put("name","张三丰1");
                 database.insert("person",null,values);
                 Toast.makeText(getApplicationContext(),"插入数据完毕",Toast.LENGTH_LONG).show();
                 break;
             case R.id.button2:
+                ContentValues values1 = new ContentValues();
+                values1.put("name", "张思凡");
+                database.update("person", values1, "name = ?", new String[]{"张三丰"});
+                Toast.makeText(getApplicationContext(),"更新数据成功", Toast.LENGTH_LONG).show();
                 break;
             case R.id.button3:
                 break;
             case R.id.button4:
+                break;
+            case R.id.button5:
+                Cursor cursor = database.query("person", null, null, null,null,null, null);
+                StringBuilder stringBuilder = new StringBuilder();
+                if (cursor.moveToFirst()) {
+                    do {
+                        int pid = cursor.getInt(cursor.getColumnIndex("personid"));
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        stringBuilder.append("id:" + pid + ";" + "name:" + name);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+                Toast.makeText(getApplicationContext(), stringBuilder.toString(), Toast.LENGTH_LONG).show();
                 break;
         }
     }
