09bf961d7e098fcb6c7cbbea5124ed8ec1e29573
waitwalker <waitwalker@163.com>
2020-08-18 14:48:25 +0800

    普通样式通知发送&取消封装

--- notification/app/src/main/java/com/example/notification/MainActivity.java
+++ notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioAttributes;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnClickListener {
 
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
+    /**
+     * 通知基于安卓8.0以上
+     *
+     * */
+
+    // views
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+
+    // Notification
+    private String kChannelID_1 = "Channel_ID_1";
+    private String kChannelName_1 = "通知频道1";
+    private String kChannelID_2 = "Channel_ID_2";
+    private String kChannelName_2 = "通知频道2";
+
+    private int commonNotificationID = 0x11;
+
+
+    private Notification commonNotification;
+    private NotificationManagerCompat commonManagerCompat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        commonNotification();
     }
 
     ///
@@ -48,8 +78,10 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button1:
+                sendCommonNotification();
                 break;
             case R.id.button2:
+                cancelCommonNotification();
                 break;
             case R.id.button3:
                 break;
@@ -58,8 +90,63 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         }
     }
 
+    ///
+    /// @name commonNotification
+    /// @description 普通样式封装
+    /// @author liuca
+    /// @date 2020/8/18
+    ///
     private void commonNotification() {
 
+        // 1. 通过NotificationManager 创建channelID
+        NotificationManager manager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel(kChannelID_1, kChannelName_1, NotificationManager.IMPORTANCE_HIGH);
+        channel.setAllowBubbles(true);
+        if (manager != null) {
+            manager.createNotificationChannel(channel);
+        }
+
+        // 2. 设置点击事件
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        resultIntent.putExtra("name","张三");
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // 3. 设置Notification的各种属性并显示
+        commonNotification = new NotificationCompat.Builder(this.getApplicationContext(), kChannelID_1)
+                .setSound(Settings.System.DEFAULT_ALARM_ALERT_URI)
+                .setSmallIcon(R.mipmap.ic_launcher_round)//设置通知图标
+                .setTicker("有新的消息!!!!!")//设置通知栏的标题
+                .setContentTitle("这个是标题")//设置标题
+                .setContentText("这个是内容")//设置内容
+                .setDefaults(NotificationCompat.DEFAULT_ALL)//设置默认提示音
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)//设置通知的优先级
+                .setOngoing(false)//让通知左右滑的时候不能取消
+                .setWhen(System.currentTimeMillis())//设置通知时间,默认为系统发出的通知的时间,通常不用设置
+                .setAutoCancel(false)//打开程序后图标是否消失
+                .setContentIntent(pendingIntent) //设置pending intent
+                .build();
+        commonManagerCompat = NotificationManagerCompat.from(this);
+    }
+
+    ///
+    /// @name sendCommonNotification
+    /// @description 发送普通样式的通知
+    /// @author liuca
+    /// @date 2020/8/18
+    ///
+    private void sendCommonNotification() {
+        commonManagerCompat.notify(commonNotificationID, commonNotification);
+    }
+
+    ///
+    /// @name cancelCommonNotification
+    /// @description 取消普通样式消息
+    /// @author liuca
+    /// @date 2020/8/18
+    ///
+    private void cancelCommonNotification() {
+        commonManagerCompat.cancel(commonNotificationID);
     }
 
 }
