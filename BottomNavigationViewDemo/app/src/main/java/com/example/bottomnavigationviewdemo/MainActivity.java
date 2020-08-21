79ea1ebc80ce6dc052bb19d2d31ac64adcdc0ed0
waitwalker <waitwalker@163.com>
2020-08-21 15:50:32 +0800

    Fragment添加

--- BottomNavigationViewDemo/app/src/main/java/com/example/bottomnavigationviewdemo/MainActivity.java
+++ BottomNavigationViewDemo/app/src/main/java/com/example/bottomnavigationviewdemo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.bottomnavigationviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView mBottomNavigationView;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        mViewPager = findViewById(R.id.viewpager);
+    }
+
+    private void initData() {
+
     }
 }
---
+++ BottomNavigationViewDemo/app/src/main/java/com/example/bottomnavigationviewdemo/fragment/VideoFragment.java
@@ -0,0 +1,40 @@
+package com.example.bottomnavigationviewdemo.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bottomnavigationviewdemo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link VideoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VideoFragment extends Fragment {
+
+    public VideoFragment() {
+        // Required empty public constructor
+    }
+
+    public static VideoFragment newInstance() {
+        VideoFragment fragment = new VideoFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
