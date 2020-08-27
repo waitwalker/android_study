c955d638066c2cfda60843a60da38b5fa610ac08
waitwalker <waitwalker@163.com>
2020-08-27 10:29:51 +0800

    添加注释

--- ActionBarDemo/app/src/main/java/com/example/actionbardemo/MainActivity.java
+++ ActionBarDemo/app/src/main/java/com/example/actionbardemo/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 下面这个方法是父类AppCompatActivity中方法
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
