9a25e6a044ddf6e21285f7d854f00b99cf0d3f80
waitwalker <waitwalker@163.com>
2020-08-27 10:00:14 +0800

    标题栏处理

--- ActionBarDemo/app/src/main/java/com/example/actionbardemo/MainActivity.java
+++ ActionBarDemo/app/src/main/java/com/example/actionbardemo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.actionbardemo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getActionBar();
+        ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle("标题");
+            actionBar.show();
 
         }
 
